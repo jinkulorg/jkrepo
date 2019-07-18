075707bc6b74445f7737e41a2c1e4f4803a1eff1
Kuldeep Panchal <52862413+jinkul@users.noreply.github.com>
2019-07-18 16:07:34 +0530

    Added firstname n lastname column in register page.

--- panchalconnect/vendor/composer/autoload_static.php
+++ panchalconnect/vendor/composer/autoload_static.php
@@ -388,6 +388,7 @@ class ComposerStaticInit788e8382b0e970350724df767cdc6402
         'App\\Http\\Controllers\\Auth\\ResetPasswordController' => __DIR__ . '/../..' . '/app/Http/Controllers/Auth/ResetPasswordController.php',
         'App\\Http\\Controllers\\Auth\\VerificationController' => __DIR__ . '/../..' . '/app/Http/Controllers/Auth/VerificationController.php',
         'App\\Http\\Controllers\\Controller' => __DIR__ . '/../..' . '/app/Http/Controllers/Controller.php',
+        'App\\Http\\Controllers\\HomeController' => __DIR__ . '/../..' . '/app/Http/Controllers/HomeController.php',
         'App\\Http\\Kernel' => __DIR__ . '/../..' . '/app/Http/Kernel.php',
         'App\\Http\\Middleware\\Authenticate' => __DIR__ . '/../..' . '/app/Http/Middleware/Authenticate.php',
         'App\\Http\\Middleware\\CheckForMaintenanceMode' => __DIR__ . '/../..' . '/app/Http/Middleware/CheckForMaintenanceMode.php',
