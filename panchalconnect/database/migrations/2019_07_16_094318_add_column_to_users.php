c823483de7667a9ef096f6dd55bb69ba51f2d110
Kuldeep Panchal <52862413+jinkul@users.noreply.github.com>
2019-07-19 18:17:25 +0530

    Correcting the classname from AddphpColumnToUsers to AddColumnToUsers

--- panchalconnect/database/migrations/2019_07_16_094318_add_column_to_users.php
+++ panchalconnect/database/migrations/2019_07_16_094318_add_column_to_users.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddphpColumnToUsers extends Migration
+class AddColumnToUsers extends Migration
 {
     /**
      * Run the migrations.
