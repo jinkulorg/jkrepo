00b42dc1cb23271fbecc120b42f183e3ebda3ea7
panchalkuldeep <57432126+panchalkuldeep@users.noreply.github.com>
2019-11-13 18:27:22 +0530

    Adding the About us content.

--- panchalconnect/resources/views/about.blade.php
+++ panchalconnect/resources/views/about.blade.php
@@ -11,13 +11,45 @@
      </ul>
    </div>
    <div class="about">
-   	  <div class="col-md-6 about_left">
    	  	<img src="" class="img-responsive" alt=""/>
-   	  </div>
-   	  <div class="col-md-6 about_right">
-			 <h1>About us</h1>
-			<p> To Do............ </p>
-   	  </div>
+        <h1>About us</h1>
+        <br>
+        <p align="justify"> <b>Panchal Connect</b> is the platform made for the people especially for Panchal community. <br><br>
+In Panchal community, there would be many boys and girls who are looking for their life partner. 
+There are also many relatives who are looking for boy or girl for someone in their family. 
+We are here to help those people in their search and they could find the appropriate girl or boy in their own community using our application. 
+We know that this world is now becoming dynamic in nature where people are started believing the concept of marriage in other caste. 
+I am not against it but I would like to say that even those who believe so, are having the feeling hidden inside them that 
+it would be better and safe if their child gets married in their own caste. 
+The reasons of this feeling are their personal attachment to the caste and 
+it seems safer to be connected with own caste rather than the other caste as same caste are familiar in many ways and couple can get along very easily.
+<br><br>
+We also believe in the relations with the people in the society which can help in making marriage successful. 
+For instance, when someone visits others house for arrange marriage of their children, both the families usually talk 
+about the people who they know in the society and try to create some link with the other family through their common known 
+relatives so that they can be connected. 
+<br><br>
+Having considered this, Panchal Connect will help you to achieve the same. 
+We have brought you the similar kind of feature on this platform, Panchal Connect, which will help you to connect the same way. 
+This feature is known as references, which you can add multiple of it and can also modify it as and when required. 
+This references will help you to find the life partner on whom you can build trust by knowing that there is one or more references 
+which are common to both the partners profile. This will create trust and also create connection link between each other and most importantly 
+one will feel safe to get connected with others and proceed with the marriage. 
+This will turn out happy marriage without worrying anything. 
+This is the only platform where you can find this kind of unique functionality all over the internet.
+<br><br>
+To create something like this for our community was our dream and we would try our best to provide you with all support 
+that is required in using this. And we assure you that we will constantly monitoring this application and maintain it well. 
+Our primary values are to help people in various ways, so we are starting with this matrimonial application but we will also be 
+looking forward to other ways where we can help in our community other than this matrimonial application in future.
+<br><br><br>
+Our Team,
+<br><br>
+<ul>
+<li>--> Kuldeep Panchal - (Founder of Panchal Connect)</li><br>
+<li>--> Jinkal Panchal - (Co-Founder of Panchal Connect)</li>
+</ul>
+ </p>
    	  <div class="clearfix"> </div>
    </div>
   </div>
